080b5bb0d9484846cf05488cef32a868bae1fe75
Svetlana Govyazina <govyazina@gmail.com>
2023-04-05 13:15:28 +0300

    new tests added

--- index.test.js
+++ index.test.js
@@ -3,4 +3,8 @@ const anagrams = require('./index')
 test('test for anagrams', () => {
     expect(anagrams('melon', 'lemon')).toBeTruthy()
     expect(anagrams('melon', 'watermelon')).toBeFalsy()
+    expect(anagrams('melon', 'melon')).toBeTruthy()
+    expect(anagrams('MELON', 'lemon')).toBeTruthy()
+    expect(anagrams('abcde2', 'c2abed')).toBeTruthy()
+    expect(anagrams('meeow', 'melon')).toBeFalsy()
 })
