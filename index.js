8af7d206ceb67b9d66a0d9b0eb66e8c798f623c2
Svetlana Govyazina <govyazina@gmail.com>
2023-04-05 12:19:47 +0300

    first test-case done)

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 // и выводит true, если строки являются анаграммами, и false, если строки ими не являются.
 
 function isAnagrams(str1, str2){
+    if (str1.length !== str2.length) {
+        return false
+    }
+    return true
 
 }
 
--- index.test.js
+++ index.test.js
@@ -2,4 +2,5 @@ const anagrams = require('./index')
 
 test('test for anagrams', () => {
     expect(anagrams('melon', 'lemon')).toBeTruthy()
+    expect(anagrams('melon', 'watermelon')).toBeFalsy()
 })
