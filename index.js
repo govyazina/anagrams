850df1899e667009d7dde0ba75481ead6961ba56
Svetlana Govyazina <govyazina@gmail.com>
2023-04-05 13:52:52 +0300

    function upgraded for tests

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function isAnagrams(str1, str2){
     if (str1.length !== str2.length) {
         return false
     }
+    const arr1 = str1.split('').sort().map(el => el.toLowerCase())
+    const arr2 = str2.split('').sort().map(el => el.toLowerCase())
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false
+        }
+    }
     return true
 
 }
