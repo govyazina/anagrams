2f92e44d44c9d97ab45b3d5aca54c305a95fc216
Svetlana Govyazina <govyazina@gmail.com>
2023-04-06 10:21:40 +0300

    function optimized

--- index.js
+++ index.js
@@ -4,19 +4,14 @@
 function isAnagrams(str1, str2) {
     str1 = str1.replace(/\W/g, '')
     str2 = str2.replace(/\W/g, '')
-    console.log(str1)
     if (str1.length !== str2.length) {
         return false
     }
-    const arr1 = str1.toLowerCase().split('').sort()
-    const arr2 = str2.toLowerCase().split('').sort()
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return false
-        }
-    }
-    return true
+    const strSorted1 = str1.toLowerCase().split('').sort().join('')
+    const strSorted2 = str2.toLowerCase().split('').sort().join('')
+    return strSorted1 === strSorted2
 
 }
+
 
 module.exports = isAnagrams
